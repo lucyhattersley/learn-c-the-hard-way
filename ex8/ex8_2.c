636717a016b44881278c1f1874f2aa24152feaa7
lucyhattersley <lucy.a.hattersley@gmail.com>
2016-01-23 19:35:27 +0000

    Added untracked files

---
+++ ex8/ex8_2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+	char none[] = "";	
+	char one[] = "a";
+
+	printf("The size of 'none': %ld.\n", sizeof(none));
+	printf("The size of 'one': %ld.\n", sizeof(one));
+}
