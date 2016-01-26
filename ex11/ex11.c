cc81263b30b050a1ca2f110cd7147df77df00029
lucyhattersley <lucy.a.hattersley@gmail.com>
2016-01-26 10:12:38 +0000

    WIP: doing extra credit on ex11

--- ex11/ex11.c
+++ ex11/ex11.c
@@ -4,19 +4,24 @@ int main(int argc, char *argv[])
 {
 	// go through each string in argv
 
-	int x = argc;
-	printf("x: %s\n", argv[x]);
-
-	int i = 0;
-	while(i < argc) {
+	int i = argc -1;
+	while(i >= 0) {
 		printf("arg %d: %s\n", i, argv[i]);
-		i++;
+		i --;
 	}
 
 	// let's make our own array of strrings
 	char *states[] = {
 		"California", "Oregon", "Washington", "Texas"
 	};
+
+
+	// WIP: Trying to add strings from argv to states
+	int num_states = argc -1;
+	i = 0;
+	while(num_states >= i) {
+		
+	}
 
 	int num_states = 3;
 	i = 0; // watch for this
