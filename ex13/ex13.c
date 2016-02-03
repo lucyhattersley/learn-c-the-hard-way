c8bf677bc3658752de2fb0043c479083ccfc2c49
lucyhattersley <lucy.a.hattersley@gmail.com>
2016-02-03 17:48:09 +0000

    ex13: changed switch to if-else

--- ex13/ex13.c
+++ ex13/ex13.c
@@ -16,38 +16,35 @@ int main(int argc, char *argv[])
 			letter = letter + 32;
 		}
 		
-		switch(letter) {
-			case 'a':
-				printf("%d: 'A'\n", i);
-				break;
+		else if(letter == 'a') {
+			printf("%d: 'A'\n", i);
+		}
 		
-			case 'e':
-				printf("%d: 'E'\n", i);
-				break;
+		else if(letter == 'e') {
+			printf("%d: 'E'\n", i);
+		}
 			
-			case 'i':
-				printf("%d: 'I'\n", i);
-				break;
+		else if(letter == 'i') {
+			printf("%d: 'I'\n", i);
+		}
 					
-			case 'o':
-				printf("%d: 'O'\n", i);
-				break;
-			
-			case 'u':
-				printf("%d: 'U'\n", i);
-				break;
-			
-			case 'y':
-				if(i > 2) {
-					// it's only sometimes Y
-					printf("%d: 'Y'\n", i);
-				}
-				break;
-			
-			default:
-				printf("%d: %c is not a vowel\n", i, letter);
+		else if(letter == 'o') {
+			printf("%d: 'O'\n", i);
+		}
+		
+		else if(letter == 'u') {
+			printf("%d: 'U'\n", i);
+		}
+		
+		else if(letter == 'y') {
+			if(i > 2) {
+				// it's only sometimes Y
+				printf("%d: 'Y'\n", i);
+			}
+		}
+		else {
+			printf("%d: %c is not a vowel\n", i, letter);
 		}
 	}
-	
 	return 0;
 }
