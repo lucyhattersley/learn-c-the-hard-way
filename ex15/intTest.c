507a12c93bd6040da0d3d62eca37c122a12a5d0f
LUCY HATTERSLEY <lucy.a.hattersley@gmail.com>
2016-02-13 18:31:47 +0000

    Added ex15 files

---
+++ ex15/intTest.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	printf("The size of an int is: %lu\n", sizeof(int));
+}
---
+++ ex15/pointerTest.c
@@ -0,0 +1,17 @@
+// Finish this code
+// Instructions http://beginnersbook.com/2014/01/c-pointer-to-pointer/
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+
+	int num=123;
+	
+	/*pointer for num*/
+	int *pr2;
+	
+	/*Double pointer for pr2*/
+	
+
+}
