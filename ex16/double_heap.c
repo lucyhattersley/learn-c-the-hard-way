8002357c3276a943cf344ac2d81f7dcff064a320
lucyhattersley <lucy.a.hattersley@gmail.com>
2016-04-28 09:55:24 +0100

    working on stack vs heap tutorial

---
+++ ex16/double_heap.c
@@ -0,0 +1,34 @@
+// Working through this tutorial
+// http://gribblelab.org/CBootcamp/7_Memory_Stack_vs_Heap.html
+
+#include <stdio.h>
+#include <stdlib.h>
+
+double *multiplyByTwo (double *input) {
+    double *twice = malloc(sizeof(double));
+    *twice = *input * 2.0;
+    return twice;
+}
+
+int main (int argc, char *argv[])
+{
+    int *age = malloc(sizeof(int));
+    *age = 30;
+    double *salary = malloc(sizeof(double));
+    *salary = 12345.67;
+    double *myList = malloc(3 * sizeof(double));
+    myList[0] = 1.2;
+    myList[1] = 2.3;
+    myList[2] = 3.5;
+
+    double *twiceSalary = multiplyByTwo(salary);
+
+    printf("double your salary is %.3f\n", *twiceSalary);
+
+    free(age);
+    free(salary);
+    free(myList);
+    free(twiceSalary);
+
+    return 0;
+}
