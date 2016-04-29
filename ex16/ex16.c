743b6af425dffa327ef3b67fedb54f2508a4a0d0
lucyhattersley <lucy.a.hattersley@gmail.com>
2016-04-29 10:44:55 +0100

    ex16 extra credit. Converted heap to stack

--- ex16/ex16.c
+++ ex16/ex16.c
@@ -19,6 +19,18 @@ struct Person {
     int weight;
 }; // note the ';' at the end of the struct definition
 
+struct Person Stack_create(char *name, int age, int height, int weight)
+{
+    struct Person who;
+
+    who.name = strdup(name);
+    who.age = age;
+    who.height = height;
+    who.weight = weight;
+
+    return who;
+}
+
 // this is where shizzle gets real complex
 // This is a function used to create an instance of the struct (defined above)
 // It allocates the memory to the struct
@@ -67,6 +79,15 @@ void Person_print(struct Person *who)
     printf("\tWeight %d\n", who->weight);
 }
 
+void Stack_print(struct Person who)
+{
+    printf("Name: %s\n", who.name);
+    printf("\tAge: %d\n", who.age);
+    printf("\tHeight %d\n", who.height);
+    printf("\tWeight %d\n", who.weight);
+}
+
+
 int main(int argc, char *argv[])
 {
     // make two people structures
@@ -96,6 +117,15 @@ int main(int argc, char *argv[])
     // destroy them both so we clean up
     Person_destroy(joe);
     Person_destroy(frank);
+
+    // now to make stack person
+    printf("------\n");
+    printf("Making stack person\n");
+    struct Person lucy = Stack_create(
+        "Lucy Hattersley", 29, 170, 69);
+
+    printf("Lucy is at memory location %p:\n", &lucy);
+    Stack_print(lucy);
 
     return 0;
 }
